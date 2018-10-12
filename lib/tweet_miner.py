c53749bf81cd409d57755b22ad59af07d5408db4
Suhail Parmar <suhailparmar@zenyatta.lan>
2018-10-12 13:04:36 +0100

    Added datetime block

--- lib/tweet_miner.py
+++ lib/tweet_miner.py
@@ -32,14 +32,31 @@ class TweetMiner:
         motorway_number = motorway.replace("M", "", 1)
         return motorway_number
 
-    def convert_datetime_to_isoformat(self):
+    def convert_datetime_to_timeblock(self):
         """
         Typical twitter_datetime = (Wed Oct 10 19:13:35 +0000 2018)
         """
         tweet_datetime = self.tweet["created_at"]
+        # Lazy check correct format
+        if len(tweet_datetime.split(" ")) != 6:
+            print('Couldnt extract datetime from {}'.format(
+                tweet_datetime))
+            raise ValueError
+
+        time_block = {}
+        time_block["day_worded"] = tweet_datetime.split(" ")[0]
+        # Convert into python datetime to easily extract minutes, hour etc
         time_pattern = strptime(tweet_datetime, "%a %b %d %H:%M:%S +0000 %Y")
         dt = datetime.fromtimestamp(mktime(time_pattern))
-        return dt.isoformat()
+
+        time_block["timestamp"] = dt.isoformat()
+        time_block["day_numerical"] = dt.day
+        time_block["year"] = dt.year
+        time_block["hour"] = dt.hour
+        time_block["minutes"] = dt.minute
+        time_block["seconds"] = dt.second
+
+        return time_block
 
 
 class Event:
@@ -47,15 +64,16 @@ class Event:
     def __init__(self):
         self.event = {
             "motorway": -1,
-            "timestamp": "",
             "id": -1,
+            # Time block
+            "timestamp": "",
             "day_numerical": -1,
             "day_worded": "",
             "year": -1,
             "hour": -1,
             "minutes": -1,
             "seconds": -1,
-            "miliseconds": -1,
+            # Data block
             "data": {
                 "junction": -1,
                 "direction": "",  # N,E,S,W
--- tests/test_tweet_miner.py
+++ tests/test_tweet_miner.py
@@ -18,8 +18,27 @@ class TestTweetMiner:
         with(raises(LookupError)):
             assert self.tm.get_motorway_number()
 
-    def test_converts_datetime_to_isoformat(self):
+    def test_convert_datetime_to_timeblock(self):
         self.tm.tweet = {"created_at": "Wed Oct 10 19:13:35 +0000 2018"}
-        assert self.tm.convert_datetime_to_isoformat() == "2018-10-10T19:13:35"
-        self.tm.tweet = {"created_at": "Mon May 11 16:02:30 +0000 1997"}
-        assert self.tm.convert_datetime_to_isoformat() == "1997-05-11T16:02:30"
+        assert self.tm.convert_datetime_to_timeblock() ==\
+            {
+            "timestamp": '2018-10-10T19:13:35',
+            "day_numerical": 10,
+            "day_worded": "Wed",
+            "year": 2018,
+            "hour": 19,
+            "minutes": 13,
+            "seconds": 35
+        }
+
+        self.tm.tweet = {"created_at": "Thu Apr 06 15:24:15 +0000 2017"}
+        assert self.tm.convert_datetime_to_timeblock() ==\
+            {
+            "timestamp": '2017-04-06T15:24:15',
+            "day_numerical": 6,
+            "day_worded": "Thu",
+            "year": 2017,
+            "hour": 15,
+            "minutes": 24,
+            "seconds": 15
+        }
