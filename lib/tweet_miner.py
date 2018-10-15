c95d10110b6598b41a3b64f337b9093a9a6fe5e6
Suhail Parmar <suhailparmar@zenyatta.lan>
2018-10-15 22:17:36 +0100

    Finished tweet miner

--- lib/tweet_miner.py
+++ lib/tweet_miner.py
@@ -2,75 +2,37 @@ from re import compile
 from time import strptime, mktime
 from datetime import datetime
 from lib.utils import Utils
+from lib.file_handler import FileHandler
 
 
 class TweetMiner:
     """
     The aim of this class is to split the tweet into seperate
-    data fields.
+    data fields, to form a canonical event.
     """
 
     def __init__(self, tweet):
         self.tweet = tweet  # {created_at: x, id: x, payload: x}
-        self.event = Event
 
-        """
-        self.location = ""
-        self.reason = ""
-        self.further_info = ""
+        # A typical Traffic_Mx payload is split into 3 sections:
+        # "(Location) - (Reason) - (Link to further info)
+        # Saves computation to split the tweet's payload once
+        self.payload_location = ""
+        self.payload_reason = ""
+        self.payload_further_info = ""
         self.__split__()
-        """
 
-    """
     def __split__(self):
 
-        A typical Traffic_Mx tweet is split into 3 sections:
-        "(Location) - (Reason) - (Link to further info)
-        The location
-
-
-        if "payload" in self.tweet:  # Prevent KeyError
+        if "payload" not in self.tweet:  # Prevent KeyError
+            print('Unable to split tweet...')
+        else:
             payload = self.tweet["payload"]
+            # Spaced to prevent splitting cities e.g. Stoke-On-Trent
             arr = payload.split(" - ")
-            self.location = arr[0]
-            self.reason = arr[1]
-            self.further_info = arr[2]
-    """
-
-    def get_direction_of_incident(self):
-        """
-        direction: (east/north/west/south)bound
-        """
-        value = self.tweet["payload"]
-        pattern = compile("[a-z]{4,5}bound")
-        direction = pattern.search(value)
-
-        if not direction:
-            print('Couldnt extract direction from payload:{}'.format(
-                value))
-            raise LookupError
-
-        direction = direction.group(0)
-        return direction[0]  # The first letter suffices (nesw)
-
-    def get_motorway_number(self, tweet_field="screen_name"):
-        """
-        tweet_field = ["screen_name", payload"]
-        Motorway number can be derived from the payload or screen_name
-        """
-        value = self.tweet[tweet_field]
-        # Strip everything apart from a MX or MXX
-        pattern = compile("[M][0-9]{1,2}")
-        motorway = pattern.search(value)
-
-        if not motorway:
-            print('Couldnt extract motorway from {0}:{1}'.format(
-                tweet_field, value))
-            raise LookupError
-
-        motorway = motorway.group(0)
-        motorway_number = motorway.replace("M", "", 1)
-        return motorway_number
+            self.payload_location = arr[0]
+            self.payload_reason = arr[1]
+            self.payload_further_info = arr[2]
 
     def convert_datetime_to_timeblock(self):
         """
@@ -84,75 +46,105 @@ class TweetMiner:
             raise ValueError
 
         time_block = {}
-        time_block["day_worded"] = tweet_datetime.split(" ")[0]
+        time_block["time_day_worded"] = tweet_datetime.split(" ")[0]
         # Convert into python datetime to easily extract minutes, hour etc
         time_pattern = strptime(tweet_datetime, "%a %b %d %H:%M:%S +0000 %Y")
         dt = datetime.fromtimestamp(mktime(time_pattern))
 
-        time_block["timestamp"] = dt.isoformat()
-        time_block["day_numerical"] = dt.day
-        time_block["year"] = dt.year
-        time_block["hour"] = dt.hour
-        time_block["minutes"] = dt.minute
-        time_block["seconds"] = dt.second
+        time_block["time_timestamp"] = dt.isoformat()
+        time_block["time_day_numerical"] = dt.day
+        time_block["time_year"] = dt.year
+        time_block["time_hour"] = dt.hour
+        time_block["time_minutes"] = dt.minute
+        time_block["time_seconds"] = dt.second
 
         return time_block
 
+    def get_direction_of_incident(self):
+        """
+        direction: (east/north/west/south)bound
+        """
+        value = self.payload_location
+        pattern = compile("[a-z]{4,5}bound")
+        direction = pattern.search(value)
+
+        if not direction:
+            print('Couldnt extract direction from payload:{}'.format(
+                value))
+            raise LookupError
+
+        direction = direction.group(0)
+        return direction[0]  # The first letter suffices (nesw)
+
+    def get_id(self):
+        return self.tweet["id"]
+
+    def get_motorway_number(self, tweet_field="screen_name"):
+        """
+        tweet_field = ["screen_name", payload"]
+        Motorway number can be derived from the payload_location or screen_name
+        """
+        value = self.tweet[tweet_field]
+        # Strip everything apart from a MX or MXX
+        pattern = compile("[M][0-9]{1,2}")
+        motorway = pattern.search(value)
+
+        if not motorway:
+            print('Couldnt extract motorway from {0}:{1}'.format(
+                tweet_field, value))
+            raise LookupError
+
+        motorway = motorway.group(0)
+        motorway_number = motorway.replace("M", "", 1)
+        return int(motorway_number)
+
+    def get_reason_for_incident(self):
+        return self.payload_reason.lower()
+
     def get_reported_junction(self):
-        payload = self.tweet["payload"]
 
-        # Spaced to prevent splitting cities e.g. Stoke-On-Trent
-        split_payload = payload.split(" - ")
-        location = split_payload[0]
-
-        # Strip everything apart from a JX or JXX
         pattern = compile("[J][0-9]{1,2}")
-        junctions = pattern.findall(location)
+        # Strip everything apart from a JX or JXX
+        junctions = pattern.findall(self.payload_location)
 
         if len(junctions) < 1:
             print('Couldnt extract junction from payload: {}'.format(
-                payload))
+                self.payload_location))
             raise LookupError
+
+        for i, junc in enumerate(junctions):
+            j = junc.replace("J", "")
+            junctions[i] = int(j)
 
         return junctions
 
-    def get_closest_city(self):
-        payload = self.tweet["payload"]
+    def get_nearest_cities(self):
 
-        # Spaced to prevent splitting cities e.g. Stoke-On-Trent
-        split_payload = payload.split(" - ")
-        location = split_payload[0]
-
-        nearest_cities = Utils.extract_contents_of_nested_brackets(location)
+        nearest_cities = Utils.extract_contents_of_nested_brackets(
+            self.payload_location)
 
         if len(nearest_cities) < 1:
             print('Couldnt extract nearest_cities from payload: {}'.format(
-                payload))
+                self.payload_location))
             raise LookupError
 
         return nearest_cities
 
+    def return_event_from_tweet(self):
+        event = {}
 
-class Event:
-    # The mined tweet output
-    def __init__(self):
-        self.event = {
-            "motorway": -1,
-            "id": -1,
-            # Time block
-            "timestamp": "",
-            "day_numerical": -1,
-            "day_worded": "",
-            "year": -1,
-            "hour": -1,
-            "minutes": -1,
-            "seconds": -1,
-            # Data block
-            "data": {
-                "junction": [],  # Could be multiple Junctions affected
-                "direction": "",  # N,E,S,W
-                "closest_cities": [],  # if multiple junctions there'll be multiple close cities
-                "incident": "",
-                "link": ""
-            }
-        }
+        event["metadata"] = "Event Generated by Tweet Miner at {}".format(
+            datetime.utcnow().isoformat())
+
+        event["motorway"] = self.get_motorway_number()
+        event["id"] = self.get_id()
+        event["junction"] = self.get_reported_junction()
+        event["direction"] = self.get_direction_of_incident()
+        event["closest_cities"] = self.get_nearest_cities()
+        event["reason"] = self.get_reason_for_incident()
+        event["extra_information"] = []
+
+        time_block = self.convert_datetime_to_timeblock()
+        event.update(time_block)
+
+        return event
--- tests/test_tweet_miner.py
+++ tests/test_tweet_miner.py
@@ -3,19 +3,29 @@ from lib.tweet_miner import TweetMiner
 
 
 class TestTweetMiner:
-    # TODO Change from assignment to a[a] = b
+    """
+    For the purpose of these tests we will be assigning values directly
+    to the self.payload_ attributes. To save computation splitting the
+    string into an array for each test.
+    """
     tm = TweetMiner({"screen_name": "Traffic_M6",
+                     "id": 1,
                      "created_at": "Wed Oct 10 19:13:35 +0000 2018",
-                     "payload": ("#M6 J2 southbound exit (Coventry) -" +
-                                 "Broken down vehicle - Full details at https:/" +
-                                 "/t.co/nkWL91Ro1g (Updated every 5 minutes)")})
+                     "payload": ("#M6 J2 southbound exit (Coventry) - Broken down vehicle - Full details at https://t.co/nkWL91Ro1g (Updated every 5 minutes)")})
+
+    def test_splitting_payload(self):
+        # __split__ is called in the constructor
+        assert self.tm.payload_location == "#M6 J2 southbound exit (Coventry)"
+        assert self.tm.payload_reason == "Broken down vehicle"
+        assert self.tm.payload_further_info ==\
+            "Full details at https://t.co/nkWL91Ro1g (Updated every 5 minutes)"
 
     def test_motorway_from_screen_name(self):
-        assert self.tm.get_motorway_number() == "6"
+        assert self.tm.get_motorway_number() == 6
 
     def test_motorway_hidden_in_screen_name(self):
-        self.tm.tweet["screen_name"] = "TrafficM5asasaas"
-        assert self.tm.get_motorway_number() == "5"
+        self.tm.tweet["screen_name"] = "TrafficM6asasaas"
+        assert self.tm.get_motorway_number() == 6
 
     def test_lookup_error_in_screen_name(self):
         self.tm.tweet["screen_name"] = "SuhailParmar"
@@ -24,81 +34,105 @@ class TestTweetMiner:
 
     def test_convert_datetime_to_timeblock(self):
         self.tm.tweet["created_at"] = "Wed Oct 10 19:13:35 +0000 2018"
-        assert self.tm.convert_datetime_to_timeblock() ==\
-            {
-            "timestamp": '2018-10-10T19:13:35',
-            "day_numerical": 10,
-            "day_worded": "Wed",
-            "year": 2018,
-            "hour": 19,
-            "minutes": 13,
-            "seconds": 35
-        }
+        time_block = self.tm.convert_datetime_to_timeblock()
+
+        assert time_block["time_timestamp"] == '2018-10-10T19:13:35'
+        assert time_block["time_day_numerical"] == 10
+        assert time_block["time_day_worded"] == "Wed"
+        assert time_block["time_year"] == 2018
+        assert time_block["time_hour"] == 19
+        assert time_block["time_minutes"] == 13
+        assert time_block["time_seconds"] == 35
 
         self.tm.tweet["created_at"] = "Thu Apr 06 15:24:15 +0000 2017"
-        assert self.tm.convert_datetime_to_timeblock() ==\
-            {
-            "timestamp": '2017-04-06T15:24:15',
-            "day_numerical": 6,
-            "day_worded": "Thu",
-            "year": 2017,
-            "hour": 15,
-            "minutes": 24,
-            "seconds": 15
-        }
+        time_block = self.tm.convert_datetime_to_timeblock()
+
+        assert time_block["time_timestamp"] == '2017-04-06T15:24:15'
+        assert time_block["time_day_numerical"] == 6
+        assert time_block["time_day_worded"] == "Thu"
+        assert time_block["time_year"] == 2017
+        assert time_block["time_hour"] == 15
+        assert time_block["time_minutes"] == 24
+        assert time_block["time_seconds"] == 15
 
     def test_get_reported_junction(self):
-        self.tm.tweet["payload"] =\
-            "#M6 J2 southbound exit (Coventry) - Broken down vehicle - Full details at https://t.co/nkWL91Ro1g (Updated every 5 minutes)"
-
-        assert self.tm.get_reported_junction() == ["J2"]
+        self.tm.payload_location = "#M6 J2 southbound exit (Coventry)"
+        assert self.tm.get_reported_junction() == [2]
 
     def test_get_reported_junctions(self):
-        self.tm.tweet["payload"] =\
-            "#M6 northbound between J6 (Birmingham) and J7 (Birmingham (N) / Walsall) - Congestion - Full details at https://www.MotorwayCameras.co.uk/Traffic#M6  (Updated every 5 minutes)"
+        self.tm.payload_location =\
+            "#M6 northbound between J6 (Birmingham) and J7 (Birmingham (N) / Walsall)"
 
-        assert self.tm.get_reported_junction() == ["J6", "J7"]
+        assert self.tm.get_reported_junction() == [6, 7]
 
     def test_get_not_junction(self):
-        self.tm.tweet["payload"] =\
-            "#M6 northbound between (Birmingham) and (Birmingham (N) / Walsall) - Congestion - Full details at https://www.MotorwayCameras.co.uk/Traffic#M6  (Updated every 5 minutes)"
+        self.tm.payload_location =\
+            "#M6 northbound between (Birmingham) and (Birmingham (N) / Walsall)"
 
         with raises(LookupError):
             assert self.tm.get_reported_junction()
 
-    def test_get_direction_of_incident(self):
+    def test_get_direction_of_incident_north(self):
 
-        self.tm.tweet["payload"] =\
-            "#M6 northbound between J6 (Birmingham) and J7 (Birmingham (N) / Walsall) - Congestion - Full details at https://www.MotorwayCameras.co.uk/Traffic#M6  (Updated every 5 minutes)"
-
+        self.tm.payload_location = "#M6 northbound between J6 (Birmingham) and J7 (Birmingham (N) / Walsall)"
         assert self.tm.get_direction_of_incident() == "n"
 
-        self.tm.tweet["payload"] =\
-            "#M6 eastbound between J6 (Birmingham) and J7 (Birmingham (N) / Walsall) - Congestion - Full details at https://www.MotorwayCameras.co.uk/Traffic#M6  (Updated every 5 minutes)"
+    def test_get_direction_of_incident_east(self):
 
+        self.tm.payload_location = "#M6 eastbound between J6 (Birmingham) and J7 (Birmingham (N) / Walsall)"
         assert self.tm.get_direction_of_incident() == "e"
 
-        self.tm.tweet["payload"] =\
-            "#M6 southbound between J6 (Birmingham) and J7 (Birmingham (N) / Walsall) - Congestion - Full details at https://www.MotorwayCameras.co.uk/Traffic#M6  (Updated every 5 minutes)"
+    def test_get_direction_of_incident_south(self):
+
+        self.tm.payload_location =\
+            "#M6 southbound between J6 (Birmingham) and J7 (Birmingham (N) / Walsall)"
 
         assert self.tm.get_direction_of_incident() == "s"
 
-        self.tm.tweet["payload"] =\
-            "#M6 westbound between J6 (Birmingham) and J7 (Birmingham (N) / Walsall) - Congestion - Full details at https://www.MotorwayCameras.co.uk/Traffic#M6  (Updated every 5 minutes)"
+    def test_get_direction_of_incident_west(self):
+
+        self.tm.payload_location =\
+            "#M6 westbound between J6 (Birmingham) and J7 (Birmingham (N) / Walsall)"
 
         assert self.tm.get_direction_of_incident() == "w"
 
-    def test_get_closest_city(self):
+    def test_get_nearest_city(self):
 
-        self.tm.tweet["payload"] =\
-            "#M6 westbound J6 (Birmingham) - Congestion - Full details at https://www.MotorwayCameras.co.uk/Traffic#M6  (Updated every 5 minutes)"
+        self.tm.payload_location = "#M6 westbound J6 (Birmingham)"
 
-        assert self.tm.get_closest_city() == ["Birmingham"]
+        assert self.tm.get_nearest_cities() == ["Birmingham"]
 
-    def test_get_closest_cities(self):
+    def test_ge_nearest_cities(self):
 
-        self.tm.tweet["payload"] =\
-            "#M6 westbound between J6 (Birmingham) and J7 (Birmingham (N) / Walsall) - Congestion - Full details at https://www.MotorwayCameras.co.uk/Traffic#M6  (Updated every 5 minutes)"
+        self.tm.payload_location =\
+            "#M6 westbound between J6 (Birmingham) and J7 (Birmingham (N) / Walsall)"
 
-        assert self.tm.get_closest_city() ==\
+        assert self.tm.get_nearest_cities() ==\
             ["Birmingham", "Birmingham (N) / Walsall"]
+
+    def test_get_incident(self):
+        self.tm.payload_reason = "Broken down vehicle"
+        assert self.tm.get_reason_for_incident() == "broken down vehicle"
+
+    def test_returned_event_from_tweet(self):
+        self.tm =\
+            TweetMiner({"screen_name": "Traffic_M6",
+                        "id": 1,
+                        "created_at": "Wed Oct 10 19:13:35 +0000 2018",
+                        "payload": ("#M6 J2 southbound exit (Coventry) - Broken down vehicle - Full details at https://t.co/nkWL91Ro1g (Updated every 5 minutes)")})
+
+        event = self.tm.return_event_from_tweet()
+        assert event["motorway"] == 6
+        assert event["id"] == 1
+        assert event["junction"] == [2]
+        assert event["direction"] == 's'
+        assert event["closest_cities"] == ["Coventry"]
+        assert event["reason"] == "broken down vehicle"
+
+        assert event["time_timestamp"] == "2018-10-10T19:13:35"
+        assert event["time_day_numerical"] == 10
+        assert event["time_day_worded"] == "Wed"
+        assert event["time_year"] == 2018
+        assert event["time_hour"] == 19
+        assert event["time_minutes"] == 13
+        assert event["time_seconds"] == 35
