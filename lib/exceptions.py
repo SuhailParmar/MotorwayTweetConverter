68501e748fb3809625251341f38101d7b0be874f
zarya <suhail.parmar@gmail.com>
2018-11-17 13:51:41 +0000

    Standardise Exceptions

--- lib/exceptions.py
+++ lib/exceptions.py
@@ -2,33 +2,35 @@ import logging
 
 
 class FailurePostToAPI(Exception):
-    def __init__(self, status_code, msg=None):
-        self.sc = status_code
-        if not msg:
-            self.msg = "Failed To Post To API. HTTPStatus Code:{}".format(
-                status_code)
-        logging.error(self.msg)
+    def __init__(self, status_code):
+        self.msg = "Failed To Post Converted Tweet To API. HTTPStatus Code:{}".format(
+            status_code)
 
     def __str__(self):
         return "{}".format(self.msg)
 
 
 class MissingPayloadException(Exception):
-    def __init__(self, tweet, msg=None):
-        if not msg:
-            logging.error(
-                "MissingPayloadException: Tweet '{}'does not have a valid payload.".format(tweet))
+    def __init__(self, tweet):
+        self.msg = "Tweet '{}' is missing payload.".format(tweet)
+
+    def __str__(self):
+        return "{}".format(self.msg)
 
 
 class InvalidPayloadException(Exception):
-    def __init__(self, payload, msg=None):
-        if not msg:
-            logging.error("Payload '{}' Is Invalid. Cannot mine accurately"
-                          .format(payload))
+    def __init__(self, payload):
+        self.msg = "Payload '{}' Is Invalid. Cannot mine accurately".format(
+            payload)
+
+    def __str__(self):
+        return "{}".format(self.msg)
 
 
 class DatetimeException(Exception):
-    def __init__(self, created_at, msg=None):
-        if not msg:
-            logging.error("Can't extract datetime from created_at: {}"
-                          .format(created_at))
+    def __init__(self, created_at):
+        self.msg = "Can't extract datetime from created_at: {}".format(
+            created_at)
+
+    def __str__(self):
+        return "{}".format(self.msg)
