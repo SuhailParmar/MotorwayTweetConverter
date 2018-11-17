80914f7045910eacf41d06b97764b8a77c279393
zarya <suhail.parmar@gmail.com>
2018-11-17 17:10:49 +0000

    Include reason for response error

--- lib/exceptions.py
+++ lib/exceptions.py
@@ -2,9 +2,13 @@ import logging
 
 
 class FailurePostToAPI(Exception):
-    def __init__(self, status_code):
-        self.msg = "Failed To Post Converted Tweet To API. HTTPStatus Code:{}".format(
-            status_code)
+    def __init__(self, status_code, content=None):
+
+        if content is not None:
+            self.msg = "FPTA:{0}:Content:{1}".format(status_code, content)
+        else:
+            self.msg = "Failed To Post Converted Tweet To API. HTTPStatus Code:{}".format(
+                status_code)
 
     def __str__(self):
         return "{}".format(self.msg)
