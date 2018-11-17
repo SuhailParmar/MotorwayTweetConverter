7d9b4835f1975e9b05272399c2a1ffb96f1b8d23
zarya <suhail.parmar@gmail.com>
2018-11-17 17:11:02 +0000

    Add OAuth support

--- lib/requests.py
+++ lib/requests.py
@@ -1,22 +1,58 @@
 from lib.config import api_base_url
 from lib.config import api_post_endpoint
-from lib.config import api_port
+from lib.config import api_port, api_client_id, api_client_secret
 from lib.exceptions import FailurePostToAPI
+from json import loads
 from requests import post
+import logging
+
+mq_logger = logging.getLogger("Requests")
 
 
 class Requests():
 
     def __init__(self):
-        self.base_url = api_base_url
+        self.host = api_base_url
         self.api_port = api_port
+        self.base_url = 'http://' + self.host + \
+            ':' + str(self.api_port) + '/'
         self.post_endpoint = api_post_endpoint
-        self.uri = 'http://' + self.base_url + ':' + str(self.api_port) + '/' + self.post_endpoint
+        self.client_id = api_client_id
+        self.client_secret = api_client_secret
+
+        self.uri = self.base_url + self.post_endpoint
+        self.provider_uri = self.base_url + 'oauth2/token/'
 
     def post_to_api(self, data):
-        response = post(self.uri, json=data)
+        logging.info('Posting Event to API...')
+        token = self.get_auth_token()
+
+        response = post(self.uri, headers={
+            'Content-Type': 'application/json',
+            'Authorization': "Bearer {0}".format(token),
+            },
+            data=data)
 
         if response.status_code != 200:
-            raise FailurePostToAPI(response.status_code)
+            raise FailurePostToAPI(response.status_code, loads(response.content))
 
         return response.status_code
+
+    def get_auth_token(self):
+        """
+        Authenticate to unmocked API using test credentials
+        Pre-Req: Ensure 2 apps are registered: Read and Write
+        """
+        url = self.provider_uri
+        grant_type = "client_credentials"
+        logging.debug('Authenticating against OAUTH2 provider')
+        request = post(url,  # Request Auth token
+                       data="grant_type={0}&client_id={1}&client_secret={2}"
+                       .format(grant_type, self.client_id, self.client_secret),
+                       headers={'Content-Type': 'application/x-www-form-urlencoded'})
+
+        if request.status_code != 200:
+            raise ValueError
+
+        content = loads(request.content)
+        return content['access_token']
